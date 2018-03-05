62e710a906ddabaa57da8327fbdb9756909561a9
Tao LI <litao912002@hotmail.com>
2018-03-05 09:42:20 +0000

    Add more style

--- Zhihu/api.py
+++ Zhihu/api.py
@@ -22,6 +22,7 @@ def gen_page(news):
         '''
         <html>
         <head>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <link rel="stylesheet" type="text/css" href="$cssurl">
         </head>
         <body>
